2b001b5db7ad7b025d2aee4e0d548065394f52f9
Stoyann <stoyann.openclassrooms@gmail.com>
2022-09-07 19:40:27 +1100

    cgu & register subscription

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,27 +5,26 @@ function Footer() {
   return (
     <footer class="footer is-fixed-bottom pt-3 pb-5">
 
- <div className="footer-list">
-  <div className='collumns'>
+ <div className="footer-list columns">
+  <div className='column'>
     <p>Voir aussi</p>
     
-    <div className='collumn'> <Link className='is-3' to={'/charte'}>Charte du bon uttilisateur</Link></div>
-    <div className='collumn'> <Link className='is-3' to={'/bug-report'}>Signalez un bug</Link></div>
-    <div className='collumn'> <Link className='is-3' to={'/thanks'}>Remerciement</Link></div>
+    <div> <Link className='is-3' to={'/charte'}>Charte du bon uttilisateur</Link></div>
+    <div > <Link className='is-3' to={'/thanks'}>Remerciement</Link></div>
   </div>
-  <div className='collumns'>
+  <div className='column'>
  <p>A propos</p>
-    <div className='collumn'> <Link className='is-3' to={'/team'}>Qui sommes nous ?</Link></div>
-    <div className='collumn'> <Link className='is-3' to={'/compagny-solutions'}>Solutions entreprises</Link></div>
-    <div className='collumn'> <Link className='is-3' to={'/legal-mentions'}>Mention légales</Link></div>
-    <div className='collumn'> <Link className='is-3' to={'/cgu'}>C.G.U</Link></div>
-    <div className='collumn'> <Link className='is-3' to={'/cgv'}>C.G.V</Link></div>
+    <div> <Link className='is-3' to={'/team'}>Qui sommes nous ?</Link></div>
+    <div> <Link className='is-3' to={'/compagny-solutions'}>Solutions entreprises</Link></div>
+    <div> <Link className='is-3' to={'/legal-mentions'}>Mention légales</Link></div>
+    <div> <Link className='is-3' to={'/cgu'}>C.G.U</Link></div>
+    <div> <Link className='is-3' to={'/cgv'}>C.G.V</Link></div>
   </div>
-  <div className='collumns'>
+  <div className='column'>
   <p>Presse</p>
-    <div className='collumn'> <Link className='is-3' to={'/testimonials'}>On parle de nous</Link></div>
-    <div className='collumn'> <Link className='is-3' to={'/press-kit'}>Kit de presse</Link></div>
-    <div className='collumn'> <Link className='is-3' to={'/files'}>Flyers/Affiches/Visuels</Link></div>
+    <div> <Link className='is-3' to={'/testimonials'}>On parle de nous</Link></div>
+    <div> <Link className='is-3' to={'/press-kit'}>Kit de presse</Link></div>
+    <div> <Link className='is-3' to={'/files'}>Flyers/Affiches/Visuels</Link></div>
 
   </div>
  
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,126 @@
+import { FaCheckCircle } from 'react-icons/fa'
 
 function Register() {
   return (
-    <main>Register</main>
+    <main className="p-4">
+      <h1>Choisissez votre formule...</h1>
+      <h2 className='notification is-small is-success p-2'>Inscription gratuite pour les 1000 premiers utilisateurs !</h2>
+      <div className="container-subscription">
+        <div className="box subcription-card">
+          <div className="subcription-card-top">
+            <h3>400 XPF / mois</h3>
+            <h5>Abonement mensuel</h5>
+          </div>
+          <p>Engagement d'1 mois renouvlable Résiliable a tout moment.</p>
+          <div className="button is-primary is-small">Détails de l'offre</div>
+          <div>
+            {' '}
+            <FaCheckCircle /> Accès illimité à tous les services Krysto.
+          </div>
+
+          <ul className="subsription-card-list">
+            <li>
+              <FaCheckCircle className="mr-2" /> Gérez vos comptes
+            </li>
+            <li>
+              <FaCheckCircle className="mr-2" /> Recevoir des Krysto
+            </li>
+            <li>
+              <FaCheckCircle className="mr-2" />
+              Publier des annonces
+            </li>
+            <li>
+              <FaCheckCircle className="mr-2" />
+              Répondre aux annonces
+            </li>
+            <li>
+              <FaCheckCircle className="mr-2" />
+              Payer en Krysto
+            </li>
+            <li>
+              <FaCheckCircle className="mr-2" />
+              Echanger des Krysto
+            </li>
+          </ul>
+
+          <div className="button is-success">Choisir cette formule</div>
+        </div>
+        <div className="box subcription-card">
+          <div className="subcription-card-top">
+            <h3>Gratuit a vie !</h3>
+            <h5>Abonement PREMIUM</h5>
+          </div>
+          <div className=" tag p-3 is-primary nb-free">
+            998 comptes gratuits sont encore disponible
+          </div>
+
+          <div className="mb-2"> </div>
+          <div className="mb-2">
+            {' '}
+            En vous inscrivant dès aujourd'hui, vous bénéficiez d'un compte
+            <strong>gratuit à vie !</strong>
+          </div>
+          <div className="mb-2">
+            A partir du 1000ème membre, l'inscription deviendra payante pour les
+            nouveaux membres.
+          </div>
+          <div className="mb-2">
+            Avec votre compte premium, vous bénéficierez d'une gratuité totale
+            et illimitée sur tous les services Krysto.
+          </div>
+          <div className="button is-primary is-small mb-5">
+            Détails de l'offre
+          </div>
+          <div className="mb-2">
+            <FaCheckCircle /> Accès illimité à tous les services Krysto.
+            <p>(Dés la première annonce publiée)</p>
+          </div>
+
+          <div className="button is-success">Créer mon compte gratuit</div>
+        </div>
+        <div className="box subcription-card">
+          <div className="subcription-card-top">
+            <h3>4000 XPF / mois</h3>
+            <h5>Abonement Annuel</h5>
+            <p> 2 mois offert !</p>
+          </div>
+          <div className="button is-primary is-small mb-5">
+            Détails de l'offre
+          </div>
+          <div className="mb-2">
+            {' '}
+            <FaCheckCircle /> Accès illimité à tous les services Krysto.
+          </div>
+
+          <ul className="subsription-card-list">
+            <li>
+              <FaCheckCircle className="mr-2" /> Gérez vos comptes
+            </li>
+            <li>
+              <FaCheckCircle className="mr-2" /> Recevoir des Krysto
+            </li>
+            <li>
+              <FaCheckCircle className="mr-2" />
+              Publier des annonces
+            </li>
+            <li>
+              <FaCheckCircle className="mr-2" />
+              Répondre aux annonces
+            </li>
+            <li>
+              <FaCheckCircle className="mr-2" />
+              Payer en Krysto
+            </li>
+            <li>
+              <FaCheckCircle className="mr-2" />
+              Echanger des Krysto
+            </li>
+          </ul>
+
+          <div className="button is-success">Choisir cette formule</div>
+        </div>
+      </div>
+    </main>
   )
 }
 
