a6b9e0af295bf05fb0e6b778a6c54ff5863e8d03
Stoyann <stoyann.openclassrooms@gmail.com>
2022-09-10 18:20:56 +1100

    open street map et leflet init

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import HeroTitle from '../../components/shared/heroTitle/HeroTitle'
@@ -6,7 +8,6 @@ import Spinner from '../../components/shared/spinner/Spinner'
 import { getProfil, reset } from '../../features/user/userSlice'
 
 function Dashboard() {
-
   const { profil, isLoading, isError, isSuccess } = useSelector(
     (state) => state.user,
   )
@@ -23,22 +24,36 @@ function Dashboard() {
   useEffect(() => {
     dispatch(getProfil())
   }, [])
+
   console.log(profil)
 
-
-  if(!profil) {
-    return <Spinner/>
+  if (!profil) {
+    return <Spinner />
   } else {
-
     return (
       <main>
-    <HeroTitle
-        title={`Bienvenue sur votre dashboard ${profil.name}`}
-      />
+        <HeroTitle title={`Bienvenue sur votre dashboard ${profil.name}`} />
+        <div className="container p-6">
+          <div className="leaflet-container">
+            <MapContainer
+              className="container p-4"
+              style={{ height: '250px' }}
+              center={profil.coordinates}
+              zoom={13}
+              scrollWheelZoom={true}
+            >
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+              />
+
    
-    </main>
-  )
-}
+            </MapContainer>
+          </div>
+        </div>
+      </main>
+    )
+  }
 }
 
 export default Dashboard
