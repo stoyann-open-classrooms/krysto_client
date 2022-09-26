0b049acaae3d5407e411cdc72b89f9f8216bf88b
Stoyann <stoyann.openclassrooms@gmail.com>
2022-09-26 12:49:47 +1100

    icone offer dashboard

--- src/pages/private/trocs/Trocs.jsx
+++ src/pages/private/trocs/Trocs.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import TitlePage from '../../../components/layout/TitlePage'
+import OfferIcone from '../../../assets/icones/offer-icone-primary.png'
 function Trocs() {
   return (
     <section>
+             <TitlePage title={'Mes trocs'} icone={OfferIcone}/>
         <h1>Trocs</h1>
     </section>
   )
