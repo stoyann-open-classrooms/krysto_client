4b63cfddaca9ef3c4eac4e52c880545223c95563
Stoyann <stoyann.openclassrooms@gmail.com>
2022-09-26 15:31:44 +1100

    buttons dashborad

--- src/components/home/landing/Landing.jsx
+++ src/components/home/landing/Landing.jsx
@@ -14,11 +14,14 @@ function Landing() {
       <div className="section-inner">
         <h4>Gagnez en pouvoir d'achat</h4>
         <h2>Graçe au Krysto</h2>
+      <p>Le krysto une monnaie numrique local inspirée de la Theorie relative de la monnaie vous permettant d’echanger des biens ou des services sur la plateforme</p>
+      </div>
+      <h5>30 000 XPF offert a l'insrcription</h5>
         <Link to={'/login'} className="btn">
           <div className="hover"></div>
-          <span>s'inscrire</span>
+          <span>s'inscrire gratuitement</span>
         </Link>
-      </div>
+
    
 
 
--- src/components/search/OffersContainer.jsx
+++ src/components/search/OffersContainer.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import Spinner from '../shared/spinner/Spinner';
 import OfferItem from './offerItem/OfferItem';
 
 function OffersContainer({ offers }) {
     console.log(offers);
     return (
         <div className='offers-container'>
+
+
+
+
             {offers.offers
                 ? offers.offers.map((offer) => (
                     <OfferItem key={offer._id} offer={offer} />
                 ))
-                : ''}
+                : <Spinner/>}
         </div>
     )
 }
