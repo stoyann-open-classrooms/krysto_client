f642fe474d03cf950ee7d30c3a77a4ce72399555
Stoyann <stoyann.openclassrooms@gmail.com>
2022-09-26 15:57:32 +1100

    layout page offer

--- src/pages/offer/Offer.jsx
+++ src/pages/offer/Offer.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
-
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import Spinner from '../../components/shared/spinner/Spinner'
+import { getOffer, reset } from '../../features/offer/offerSlice'
+import './offer.css'
 function Offer() {
+
+    const params = useParams()
+
+    const { offer, isLoading, isError, isSuccess } = useSelector(
+      (state) => state.offer,
+    )
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+      return () => {
+        if (isSuccess) {
+          dispatch(reset())
+        }
+      }
+    }, [dispatch, isSuccess, isError])
+  
+    useEffect(() => {
+      dispatch(getOffer(params.id))
+    }, [])
+    console.log(offer.offer)
+
+
+if(!offer.offer) {
+    return <Spinner/>
+}
+
   return (
-    <section>Offer</section>
+    <section>
+        <div className="offer-slider-container">
+            
+        </div>
+        <div className="offer-page-top">*
+        <h1>
+         {offer.offer.title}
+        </h1>
+        <div className="price">
+            <p>{offer.offer.amountMony}</p>
+        </div>
+        </div>
+        <div className="offer-page-body">
+            <p>{offer.offer.text}</p>
+        </div>
+        <div className="offer-page-footer buttons">
+            <button className='btn-offer'>Contacter le vendeur</button>
+            <button className='btn-offer'>Faire une proposition</button>
+        </div>
+    </section>
   )
 }
 
